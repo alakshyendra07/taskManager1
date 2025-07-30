8ae457b2693d69a42e8370453f1b61c92ad39fb3
alakshyendra07 <alakshyendra.pandey@argusoft.com>
2025-07-30 14:21:13 +0530

    updated code

--- src/com/taskmanager/Main.java
+++ src/com/taskmanager/Main.java
@@ -1,12 +1,7 @@
 package com.taskmanager;
 
 import com.taskmanager.exception.TaskNotFoundException;
-import com.taskmanager.model.Task;
-import com.taskmanager.model.TimedTask;
-import com.taskmanager.model.RecurringTask;
 import com.taskmanager.service.TaskManager;
-
-
 import java.util.Scanner;
 
 public class Main {
@@ -31,62 +26,19 @@ public class Main {
             try {
                 switch (choice) {
                     case 1:
-                        System.out.print("Enter Task Description: ");
-                        String desc = sc.nextLine();
-                        sc.nextLine();
-                        System.out.println("Enter Task Type: 1.Normal Task, 2.TimedTask, 3.RecurringTask");
-                        int type = sc.nextInt();
-                        if (type == 1) {
-                            ++tid;
-                            Task t1 = new Task(tid, desc);
-                            taskManager.addTask(t1);
-                            System.out.println("TASK ADDED");
-                        } else if (type == 2) {
-                            System.out.println("Enter the duration in minutes: ");
-                            int time = sc.nextInt();
-                            ++tid;
-                            Task t1 = new TimedTask(tid, desc, time);
-                            taskManager.addTask(t1);
-                            System.out.println("TASK ADDED");
-
-                        } else if (type == 3) {
-                            System.out.println("Enter the duration in minutes: ");
-                            int time = sc.nextInt();
-                            System.out.println("Enter the repeat interval: ");
-                            int interval = sc.nextInt();
-                            ++tid;
-                            Task t1 = new RecurringTask(tid, desc, time, interval);
-                            taskManager.addTask(t1);
-                            System.out.println("TASK ADDED");
-                        }
-
-
+                        taskManager.addTask();
                         break;
 
                     case 2:
                         taskManager.viewTask();
                         break;
                     case 3:
-                        taskManager.viewTask();
-                        System.out.println("Enter the id of the task you want to mark as completed");
-                        int completedId = sc.nextInt();
-                        if (taskManager.markTaskAsDone(completedId) == 1) {
-                            System.out.println("MARKED SUCCESSFULLY");
-                        } else {
-                            System.out.println("FIELD NOT FOUND");
-                        }
+                        taskManager.markTaskAsDone();
                         break;
                     case 4:
-                        System.out.println("Enter the task id you want to delete:");
-                        int del = sc.nextInt();
-                        if (taskManager.deleteTask(del) == 1) {
-                            System.out.println("DELETED SUCCESSFULLY");
-                        } else {
-                            System.out.println("FIELD NOT FOUND");
-                        }
+                        taskManager.deleteTask();
                         break;
                     case 5:
-                        System.out.println("The completed tasks are: ");
                         taskManager.filterAndDisplayTask();
                         break;
                     case 0:
@@ -100,6 +52,6 @@ public class Main {
                 ;
             }
         }
-            while (choice!=0);
+        while (choice!=0);
     }
 }
--- src/com/taskmanager/service/TaskManager.java
+++ src/com/taskmanager/service/TaskManager.java
@@ -1,49 +1,99 @@
 package com.taskmanager.service;
 
 import com.taskmanager.enums.TaskStatus;
+import com.taskmanager.exception.TaskNotFoundException;
+import com.taskmanager.model.RecurringTask;
 import com.taskmanager.model.Task;
+import com.taskmanager.model.TimedTask;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TaskManager {
 
-    List<Task> tasks = new ArrayList<>();
+    int tid = 0;
 
-    public void addTask(Task task){
-        tasks.add(task);
+    List<Task> tasks = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public void addTask(){
+        System.out.print("Enter Task Description: ");
+        String desc = sc.nextLine();
+        sc.nextLine();
+        System.out.println("Enter Task Type: 1.Normal Task, 2.TimedTask, 3.RecurringTask");
+        int type = sc.nextInt();
+        if (type == 1) {
+            ++tid;
+            Task t1 = new Task(tid, desc);
+            tasks.add(t1);
+            System.out.println("TASK ADDED");
+        } else if (type == 2) {
+            System.out.println("Enter the duration in minutes: ");
+            int time = sc.nextInt();
+            ++tid;
+            Task t1 = new TimedTask(tid, desc, time);
+            tasks.add(t1);
+            System.out.println("TASK ADDED");
+
+        } else if (type == 3) {
+            System.out.println("Enter the duration in minutes: ");
+            int time = sc.nextInt();
+            System.out.println("Enter the repeat interval: ");
+            int interval = sc.nextInt();
+            ++tid;
+            Task t1 = new RecurringTask(tid, desc, time, interval);
+            tasks.add(t1);
+            System.out.println("TASK ADDED");
+        }
     }
 
     public void viewTask(){
         for (Task task: tasks){
-            System.out.println("view task "+ task.getDescription());
+            System.out.println(task.getDescription());
             task.getDetail();
         }
     }
 
-    public int markTaskAsDone(int id){
+    public int markTaskAsDone() throws TaskNotFoundException {
 
+        viewTask();
+        System.out.println("Enter the id of the task you want to mark as completed");
+        int completedId = sc.nextInt();
+        int res = 0;
         for (Task task : tasks) {
-            if (task.getId() == id) {
+            if (task.getId() == completedId) {
                 task.setStatus(TaskStatus.COMPLETED);
-                return 1;
+                System.out.println("MARKED SUCCESSFULLY");
+                res = 1;
+                break;
             }
+        }
+        if(res == 0){
+            throw new TaskNotFoundException("Something went Wrong");
         }
         return 0;
     }
 
-    public int deleteTask(int id){
+    public int deleteTask() throws TaskNotFoundException {
+        System.out.println("Enter the task id you want to delete:");
+        int del = sc.nextInt();
+        int res = 0;
         for (Task task : tasks) {
-            if (task.getId() == id) {
+            if (task.getId() == del) {
                 tasks.remove(task);
-                return 1;
+                System.out.println("DELETED SUCCESSFULLY");
+                res = 1;
             }
         }
+        if(res == 0) {
+            throw new TaskNotFoundException("Something went Wrong");
+        }
         return 0;
-
     }
 
     public void filterAndDisplayTask(){
+        System.out.println("The completed tasks are: ");
         for (Task task : tasks) {
             if (task.getStatus() == TaskStatus.COMPLETED) {
                 task.getDetail();
